1d87f5538f233a1ac1cec8721b3710cd576217c9
Murtaza Abidi <murtaza2476175@gmail.com>
2023-06-12 15:59:44 +0500

    First Commit

---
+++ features/user/userSlice.js
@@ -0,0 +1,41 @@
+const { default: axios } = require('axios')
+
+const createSlice = require('@reduxjs/toolkit').createSlice
+const createAsyncThunk = require('@reduxjs/toolkit').createAsyncThunk
+
+const initialState = {
+    loading: false,
+    users: [],
+    error: '',
+}
+
+// Generates pending, fulfilled and rejectedaction types
+const fetchUsers = createAsyncThunk('user/fetchUsers', () => {
+    return axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        // .get('https://jsonplaceholder.typicode.com/usersasdf') // promise rejected due to 404 url not found
+        .then((response) => response.data.map((user) => user.id))
+})
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    extraReducers: (builder) => {
+        builder.addCase(fetchUsers.pending, (state) => {
+            state.loading = true
+        })
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            state.loading = false
+            state.users = action.payload
+            state.error = ''
+        })
+        builder.addCase(fetchUsers.rejected, (state, action) => {
+            state.loading = false
+            state.users = []
+            state.error = action.error.message
+        })
+    }
+})
+
+module.exports = userSlice.reducer
+module.exports.fetchUsers = fetchUsers
